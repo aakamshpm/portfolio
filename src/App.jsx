b4018b2e548909cc90cbdf0aaca008fcdf9c7f69
Aakamsh PM <aakamshpm@gmail.com>
2025-04-30 01:54:30 +0530

    career's included

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import NavBar from "./components/NavBar";
+import CareerPath from "./sections/CareerPath";
+import FeatureCards from "./sections/FeatureCards";
 import Hero from "./sections/Hero";
 import Showcase from "./sections/Showcase";
 
 const App = () => {
   return (
     <>
+      <NavBar />
       <Hero />
       <Showcase />
+      <FeatureCards />
+      <CareerPath />
     </>
   );
 };
